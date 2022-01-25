e5cede610ab18504654555b77c0e8813a4230b1d
Berkay <berkaygumus32@gmail.com>
2022-01-24 21:49:56 -0700

    WeatherApp

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+//const { getUnpackedSettings } = require('http2');
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+
+    const query = req.body.cityName;
+    const apiKey = "eb2c4307b5f48c0d5b7a80faeeb1879e";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+
+
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon +"@2x.png";
+
+            res.write("<p>The weather is currently " + weatherDescription + " </p>");
+            res.write("<h1>The temparature in " + query + " is " + temp + " degrees Celcius</h1>");
+            res.write("<img src=" + imageURL + ">")
+            res.send();
+        });
+    });                   
+});
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000.");
+}) 
